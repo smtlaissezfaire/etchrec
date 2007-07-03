072c065c540d4c43573243b13eecd4c0c303a674
smtlaissezfaire <smtlaissezfaire@4da7c5d9-f56e-4f3a-a80e-4f09479cb4df>
2007-07-03 16:18:30 +0000

    adding recipes.rb
    git-svn-id: http://etchrec.rubyforge.org/svn/trunk@11 4da7c5d9-f56e-4f3a-a80e-4f09479cb4df

--- etchrec/lib/etchrec.rb
+++ etchrec/lib/etchrec.rb
@@ -1,6 +1,5 @@
 
-require 'rubygems'
-require 'deprec/recipes'
+require 'deprec/recipes'                                           
 require File.dirname(__FILE__) + "/capistrano_extentions/actor"
 require File.dirname(__FILE__) + "/capistrano_extentions/apt"
 require File.dirname(__FILE__) + "/recipes/etchrec"
